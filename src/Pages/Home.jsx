cfa2d8c2cd39457d8534af64c172be35dc36ae29
RogueGrim <equan4843@gmail.com>
2025-12-05 00:50:05 +0530

    styled home page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,5 +1,6 @@
 import styles from './Home.module.css'
 import img from'../resources/logo.svg'
+import Carosel from '../component/Carosel'
 
 const Home = ()=>{
 
@@ -7,7 +8,7 @@ const Home = ()=>{
         <>
             <header className={styles.header}>
                 <div className={styles.logo}>
-                    <img src={img} alt="" />
+                    <img src={img} alt="Logo"/>
                     <h1>Shopify</h1>
                 </div>
 
@@ -18,6 +19,19 @@ const Home = ()=>{
                     <h1>Cart</h1>
                 </div>
             </header>
+            <Carosel/>
+            <div className={styles.para}>
+                <h1 className={styles.title}>
+                    Shop With Us
+                </h1>
+                <p className={styles.lines}>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta enim tenetur facilis quisquam placeat perspiciatis veniam mollitia iure blanditiis, exercitationem quibusdam dolor, quasi at esse iusto harum expedita nesciunt qui?
+                </p>
+            </div>
+            <div className={styles.button}>
+                <button className={styles.buy}>Shop Now</button>
+                <button className={styles.membership}>Get Membership</button>
+            </div>
         </>
     )
 }
